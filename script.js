731525682f0ebc855e0ddaa7d17449cc317c8fcb
gabrielopes7 <gabrielplopes7@hotmail.com>
2022-04-03 12:10:14 -0300

    Balões do robo pronto e inicio da escolha da opçao

--- script.js
+++ script.js
@@ -2,21 +2,44 @@ let hora = new Date();
 let horaAtual = hora.getHours();
 let minutoAtual = hora.getMinutes();
 
-let horaMensagem = document.getElementById("balaoFala");
-
-function iniciar(){
-    let botao = document.getElementById("botaoIniciar");
-    let corpoChat = document.getElementById("corpoPrincipal");
-    let criaDiv = document.createElement("div");
-
-    criaDiv.setAttribute("id", "textoRobo");
-    corpoChat.append(criaDiv)
+function horaMensagem(h, m) {
+  let hora = document.getElementsByClassName("hora");
+  for (let i = 0; i < hora.length; i++){
+    if (m < 10) {
+     hora[i].innerHTML += h + ":0" + m;
+    } else {
+      hora[i].innerHTML += h + ":" + m;
+    }
     
-
-
-    
-
-    botao.style.display = "none";
-
+  }
 }
 
+function enter(e){
+  let key = e.which || e.keyCode;
+  if(key == 13){
+    let msgUser = document.getElementById("user").value;
+    if (msgUser == ""){
+      alert("Escolha uma opção");
+    }else{
+      switch (msgUser){
+        case "1":
+          console.log("Deu certo");
+          break;
+        case "2":
+          console.log("Correto");
+          break;
+        case "3":
+          console.log("Tô indo");
+          break
+        default:
+          console.log("Digite o valor correto")
+      }
+
+    }
+  }
+  
+  
+  
+}
+
+horaMensagem(horaAtual, minutoAtual);
