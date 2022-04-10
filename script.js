e936181bd7ba56d592fc7caca0a0478d7769c7ca
gabrielopes7 <gabrielplopes7@hotmail.com>
2022-04-10 20:57:56 -0300

    Problema sobre horas resolvido, faltam implementar a API.

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-function hora(t){
+function hora() {
   let hora = new Date();
   let horaAtual = hora.getHours();
   let minutoAtual = hora.getMinutes();
   return [horaAtual, minutoAtual];
 }
 
-// Devo criar outra função para que eu possa colocar o horário nos balões já criados.
+function horaBalaoInicial() {
+  let horaBalao = document.getElementsByClassName("hora");
+
+  let horaMinuto = hora();
+
+  if (horaMinuto[1] < 10) {
+    horaBalao[0].innerHTML = horaMinuto[0] + ":0" + horaMinuto[1];
+    horaBalao[1].innerHTML = horaMinuto[0] + ":" + horaMinuto[1];
+  } else {
+    horaBalao[0].innerHTML = horaMinuto[0] + ":" + horaMinuto[1];
+    horaBalao[1].innerHTML = horaMinuto[0] + ":" + horaMinuto[1];
+  }
+}
 
 function horaMensagem(h, m) {
   let hora = document.getElementsByClassName("hora");
 
-    if (m < 10) {
-      return hora.innerHTML = h + ":0" + m;
-    } else {
-      return hora.innerHTML = h + ":" + m;
-    }
-
+  if (m < 10) {
+    return (hora.innerHTML = h + ":0" + m);
+  } else {
+    return (hora.innerHTML = h + ":" + m);
+  }
 }
 
-function criarBaloes(){
+function criarBaloes() {
   let textoUser = document.createElement("div");
   let textoRobo = document.createElement("div");
   let fotoRobo = document.createElement("div");
@@ -39,67 +50,53 @@ function criarBaloes(){
   divHoraUser.setAttribute("class", "hora");
   divHoraRobo.setAttribute("class", "hora");
 
-  divHoraUser.innerHTML =  horaMensagem(horaMinuto[0], horaMinuto[1])
-  divHoraRobo.innerHTML =  horaMensagem(horaMinuto[0], horaMinuto[1])
+  divHoraUser.innerHTML = horaMensagem(horaMinuto[0], horaMinuto[1]);
+  divHoraRobo.innerHTML = horaMensagem(horaMinuto[0], horaMinuto[1]);
 
   corpoChat.appendChild(textoUser);
   textoUser.appendChild(divUser);
   textoUser.appendChild(divHoraUser);
-  
 
   setTimeout(() => {
     corpoChat.appendChild(textoRobo);
     textoRobo.appendChild(fotoRobo);
     textoRobo.appendChild(divRobo);
     textoRobo.appendChild(divHoraRobo);
-  },30);
+  }, 30);
 
   setTimeout(() => {
-    horaMensagem(horaMinuto[0], horaMinuto[1])
-  },40);
+    horaMensagem(horaMinuto[0], horaMinuto[1]);
+  }, 40);
 
   return [divUser, divRobo];
-  
 }
 
-
 function enter(e) {
-  
   let key = e.which || e.keyCode;
   if (key == 13) {
     let msgUser = document.getElementById("user").value;
     if (msgUser == "") {
       alert("Escolha uma opção");
-      msgUser.focus
+      msgUser.focus;
     } else {
       let chamaFuncao = criarBaloes();
 
       switch (msgUser) {
         case "1":
-
-          if(hora[0] >= 0 && hora[0] <= 12){
+          if (hora[0] >= 0 && hora[0] <= 12) {
             chamaFuncao[0].innerText = "Bom dia!";
             chamaFuncao[1].innerText = "Bom dia!";
-            
-          } else if (hora[0] > 17 && hora[0] <= 23){
+          } else if (hora[0] > 17 && hora[0] <= 23) {
             chamaFuncao[0].innerText = "Boa noite!";
             chamaFuncao[1].innerText = "Boa noite!";
-            
-          } else{
+          } else {
             chamaFuncao[0].innerText = "Boa tarde!";
             chamaFuncao[1].innerText = "Boa tarde!";
-            
           }
-          break; 
-
-
-
-
-
-
+          break;
 
         case "2":
-          chamaFuncao[0].innerText = "Recomende-me um livro, por favor!"
+          chamaFuncao[0].innerText = "Recomende-me um livro, por favor!";
 
           let livros = [
             "Em Busca do Tempo Perdido (1913-1927), de Marcel Proust",
@@ -112,22 +109,21 @@ function enter(e) {
             "Lolita (1955), de Vladimir Nabokov",
             "Hamlet (1609), de William Shakespeare",
             "O Apanhador no Campo de Centeio (1945), de J.D. Salinger",
-          ]
-          
-          chamaFuncao[1].innerText = livros[Math.floor(Math.random() * (livros.length))]
+          ];
+
+          chamaFuncao[1].innerText =
+            livros[Math.floor(Math.random() * livros.length)];
           break;
         case "3":
           const url = "http://api-public.guidebox.com/v2/";
           fetch(url)
-            .then((res)=>{
+            .then((res) => {
               return res.json();
             })
-            .then((data) =>{          // Buscar uma API que faça o uso de catálogo netflix ou prime
-              console.log(data)
-            })
-
-          
-        
+            .then((data) => {
+              // Buscar uma API que faça o uso de catálogo netflix ou prime
+              console.log(data);
+            });
 
           break;
         default:
@@ -137,5 +133,6 @@ function enter(e) {
   }
 }
 
+window.onload = horaBalaoInicial();
+
 // Próximo passo é buscar uma api de catálogo de séries para poder recomendar uma ao usúario.
-// Os balões criados estão resolvidos, agora falta colocar no balões que já estão
